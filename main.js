9ee56981347aceb866dd30e9cddf0fa4005ca10e
Alessia <alessia@claireity.io>
2024-04-26 19:52:53 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+{
+  "title": "Actor Input",
+  "type": "object",
+  "properties": {
+    "url": {
+      "title": "URL",
+      "type": "string",
+      "description": "The URL to send requests to."
+    },
+    "minTime": {
+      "title": "Minimum Time on Page",
+      "type": "integer",
+      "description": "Minimum time spent on each page in seconds."
+    },
+    "maxTime": {
+      "title": "Maximum Time on Page",
+      "type": "integer",
+      "description": "Maximum time spent on each page in seconds."
+    },
+    "numRequests": {
+      "title": "Number of Requests",
+      "type": "integer",
+      "description": "Number of requests to make."
+    },
+    "avoidRepetition": {
+      "title": "Avoid Repetition",
+      "type": "boolean",
+      "description": "Whether to avoid repeating the page view."
+    }
+  },
+  "required": ["url", "minTime", "maxTime", "numRequests"]
+}
